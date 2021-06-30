8eab962e9777a915a5d99ab505f1a307c7866da4
VinayWritesCode <vinaysuhalka49@gmail.com>
2021-06-30 23:49:34 +0530

    SearchBar Created and DateTime added in taskbar.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// open start menu code
+
 var StartIcon = document.getElementById("Start-menu");
 var browser = document.querySelector(".browser");
 
 StartIcon.addEventListener("click", function () {
     let WindowContent = document.querySelector(".window-content");
     if (WindowContent.style.bottom === "0vh") {
-        WindowContent.style.bottom = "92vh";
+        WindowContent.style.bottom = "93vh";
     }
     else {
         WindowContent.style.bottom = "0vh";
@@ -12,6 +14,39 @@ StartIcon.addEventListener("click", function () {
 });
 
 
+// Date And Time 
+
+const CurrentTime = document.getElementById("time");
+const CurrentDay = document.getElementById("day");
+const CurrentDate = document.getElementById("date");
 
 
+setInterval(() => {
+        UpdateTime();
+        UpdateDate();
+}, 1000);
 
+function UpdateDate() {
+    const DATE = new Date();
+    const date = DATE.getDay() + "/" + DATE.getMonth() + "/" + DATE.getFullYear();
+    const day = DATE.toDateString().slice(0,3) ;
+    CurrentDay.innerHTML = day;
+    CurrentDate.innerHTML = date;
+}
+
+function UpdateTime() {
+    const DATE = new Date();
+    var hours = DATE.getHours();
+    var minutes = DATE.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    var isAMorPM = "AM";
+    if (hours > 12) {
+        hours -= 12;
+        isAMorPM = "PM";
+    }
+    var time = hours + ":" + minutes + " " + isAMorPM;
+    CurrentTime.innerHTML = time;
+
+}
