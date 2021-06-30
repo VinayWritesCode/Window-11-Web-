9b6fcb1ee6a33bdf4bd7708092af407ec5dd6aa3
VinayWritesCode <vinaysuhalka49@gmail.com>
2021-06-30 13:40:12 +0530

    Added icons

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var StartIcon = document.getElementById("Start-menu");
+var browser = document.querySelector(".browser");
 
 StartIcon.addEventListener("click", function () {
     let WindowContent = document.querySelector(".window-content");
     if (WindowContent.style.bottom === "0vh") {
-        WindowContent.style.bottom = "85vh";
+        WindowContent.style.bottom = "92vh";
     }
     else {
         WindowContent.style.bottom = "0vh";
